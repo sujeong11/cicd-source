3736dfd1eda6f322374c57251f7a0577ba668db4
sujeong11 <qalzm7351@gmail.com>
2025-05-18 20:28:00 +0900

    feat: 테스트 API 작성

---
+++ src/main/java/com/project/cicd/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.project.cicd.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class TestController {
+
+  @GetMapping("/test")
+  public String test() {
+    return "CI/CD:v1";
+  }
+}
